a74f3672c99d7eda97e7fe8762b9612b42142719
jvisker <jamievisker@gmail.com>
2013-10-01 12:56:19 -0600

    Updated Database Accessing. I need states to work to finish

--- Database.java
+++ Database.java
@@ -1,100 +1,174 @@
 package database;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
 
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
-
-//import node.Node;
-
+/**
+ * Initializes the database and stores information about the database connection 
+ * and the data access classes that can be used to manipulate the information in 
+ * the database
+ * @author Mychal Calderon
+ */
 public class Database {
-	private static final String Database_File = "HyPeerWeb.sqlite";
-	private final String Database_URL = "jdbc:sqlite:" + Database_File;
 	
-	private static Database database = null;
-	
-	private File createStatements;
+	private static Database db = null;
+	private DatabaseAccessor dbAccessor;
 	private Connection connection;
+	private static String dbUrl;
 	
+	// singleton model
+	public static Database getInstance() {
+		if (db == null) {
+			db = new Database();
+			// new instance needs to be initialized
+		}
+		return db;
+	}
+	
+	// private constructor, no other class can access it
 	private Database() {
-		initialize();
-	}
-	
-	public static Database getSingleton() {
-		if(database == null) {
-			database = new Database();
-		}
-		return database;
-	}
-	
-	public void getDatabaseConnection() {
-		
-	}
-	
-	private void initialize() {
-		
-		this.createStatements = new File("createStatmentes.txt").getAbsoluteFile();
-		
-		try 
-		{
-			Class.forName("org.sqlite.JDBC");
-		}
-		
-		catch (ClassNotFoundException e1) 
-		{
-			e1.printStackTrace();
-		}
-		
-		try 
-		{
-			this.connection = DriverManager.getConnection(this.Database_URL);
-		} 
-		
-		catch (SQLException e) 
-		{
-			System.out.println("Cannot connect to the Database");
-			e.printStackTrace();
-			return;
-		}
-		createDatabase();
-		return;
-	}
-	
-	private void createDatabase() {
-		Statement stmt = null;
-		Scanner myScanner = null;
-		
-		try {
-			myScanner = new Scanner(this.createStatements);
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
-			stmt = connection.createStatement();
-			stmt.setQueryTimeout(30);
-			
-			while(myScanner.hasNextLine()) {
+		if (dbUrl != null) {
+			try {
 				
-				stmt.executeUpdate(myScanner.nextLine());
+				this.connection = DriverManager.getConnection(dbUrl);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				this.connection = null;
 			}
-			connection.close();
+		} else {
+			this.connection = null;
+		}
+		this.dbAccessor = new DatabaseAccessor(this);
+	}
+	
+	/**
+	 * Load the SQLite database driver
+	 * @throws ServerException
+	 */
+	public static void initialize() throws DatabaseException {
+		// Load the SQLite database driver
+		try {
+			// register the driver 
+			String driverName = "org.sqlite.JDBC";
+			Class.forName(driverName);
+			File currentDir = new File("");
+			String dbName = currentDir.getAbsolutePath() + "\\src\\database\\indexer_server.sqlite";
+					//+ "/database/indexer_server.sqlite";
+			
+			String jdbcDef = "jdbc:sqlite";
+			dbUrl = jdbcDef + ":" + dbName;
+			// which will produce a legitimate Url for SqlLite JDBC :
+			// jdbc:sqlite:myDatabase.db
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Database driver class could not be found.");
+			throw new DatabaseException();
+		} 
+	}
+	
+	public DatabaseAccessor getDatabaseAccessor() {
+		return dbAccessor;
+	}
+	
+	public Connection getConnection() {
+		try {
+			if (connection.isClosed()) {
+				this.connection = DriverManager.getConnection(dbUrl);
+			} 
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			try
-			{
-				if(connection != null)
-				{
-					connection.close();
+			this.connection = null;
+		}
+		return this.connection;
+	}
+	
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	/**
+	 * Open a connection to the database and start a transaction
+	 * @throws ServerException
+	 */
+	public void startTransaction() {
+		
+		// turn off auto commit to enable grouping transactions
+		try {
+			this.getConnection().setAutoCommit(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Commit or rollback the transaction and close the connection
+	 * @param commit
+	 */
+	public void endTransaction(boolean commit) {
+		// Commit or rollback the transaction
+		this.connection = this.getConnection();
+		if (this.connection != null) {
+			if (commit == true) {
+				try {
+					this.connection.commit();
+				} catch (SQLException e) {
+					// rollback the commit if something went wrong
+					try {
+		                System.err.print("Transaction is being rolled back");
+		                this.connection.rollback();
+		            } catch(SQLException excep) {
+		            	excep.printStackTrace();
+		            }
+				}
+			} else {
+				// rollback the commit if false was passed into the method
+				try {
+					this.connection.rollback();
+				} catch (SQLException e) {
+					e.printStackTrace();
 				}
 			}
-			catch(SQLException e)
-			{
-				System.err.println(e);
+			
+			// reset auto commit to true and close the connection
+			try {
+				this.connection.setAutoCommit(true);
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
 		}
+		
 	}
+	
+	/**
+	 * Drops all tables in the database and recreates them
+	 * @param file the file which contains the sql statements for creating the database
+	 */
+	public void buildTables(File file) throws SQLException {
+		try {
+			
+			BufferedReader reader = new BufferedReader( new FileReader (file));
+		    String line = null;
+		    Statement stat = this.getConnection().createStatement();
+
+		    while( (line = reader.readLine()) != null ) {
+			    stat.executeUpdate(line);
+		    }
+		    
+		    reader.close();
+		    
+		 // clean up and close
+	        stat.close(); 
+		    
+		} catch (FileNotFoundException e1) {
+			System.out.println("The sql file used to generate the database tables could not be found.");
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	    
+	}
+	
 }
--- DatabaseAccessor.java
+++ DatabaseAccessor.java
@@ -1,8 +1,11 @@
+package database;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
+
+import model.Node;
 
 
 /**
@@ -26,31 +29,107 @@ public class DatabaseAccessor {
 	 */
 	public Node getNode(int webID) throws DatabaseException, SQLException {	
 		// instantiate local variables
-		int id, height, foldID, surFoldID, invSurFoldID;
+		int id, height, foldID, surFoldID, invSurFoldID, foldState, insertableState, selfFlat, neighborsFlat, nofneighborsFlat;
 		Node node;
 		String query = "SELECT * FROM Nodes WHERE webID = ?";
 		PreparedStatement stat = null;
 		ResultSet rs = null;
 		node = null;
-		
+		try {
 		stat = db.getConnection().prepareStatement(query);
 		// set the variables in the query
 		stat.setInt(1, webID);
 
 		rs = stat.executeQuery();
 
-		// if the result set is not empty, create a Batch to return
+		
 		if (rs.next()) {
 			id = rs.getInt("webID");
     		height = rs.getInt("height");
     		foldID = rs.getInt("foldID");
     		surFoldID = rs.getInt("surFoldID");
     		invSurFoldID = rs.getInt("invSurFoldID");
-    		node = new Node(id, height, foldID, surFoldID, invSurFoldID);
+    		//foldState, insertableState, selfFlat, neighborsFlat, nofneighborsFlat
+    		foldState = rs.getInt("foldState");
+    		insertableState = rs.getInt("insertableState");
+    		selfFlat = rs.getInt("selfFlat");
+    		neighborsFlat = rs.getInt("neighborsFlat");
+    		nofneighborsFlat = rs.getInt("nofneighborsFlat");
+    	
+    		rs.close(); //this might not work. I only want to find neighbors if the webid was found, but I also need to close the connection to make a new query
+    		//having it in this if statment seems odd though.
+    		stat.close();
+    		
+    		query = "SELECT neighborID FROM Neighbors WHERE webID = ?";
+    		stat = null;
+    		rs = null;
+    		node = null;
+    		
+    		stat = db.getConnection().prepareStatement(query);
+    		// set the variables in the query
+    		stat.setInt(1, webID);
+
+    		rs = stat.executeQuery();
+    		List<Integer> neighbors = new ArrayList<Integer>(); 
+    		while (rs.next()){
+    			
+    			neighbors.add(rs.getInt("neighborID"));
+    		}
+    		rs.close(); //this might not work. I only want to find neighbors if the webid was found, but I also need to close the connection to make a new query
+    		//having it in this if statment seems odd though.
+    		stat.close();
+    		query = "SELECT surID FROM SurNeighbors WHERE webID = ?";
+    		stat = null;
+    		rs = null;
+    		node = null;
+    		
+    		stat = db.getConnection().prepareStatement(query);
+    		// set the variables in the query
+    		stat.setInt(1, webID);
+
+    		rs = stat.executeQuery();
+    		List<Integer> surNeighbors = new ArrayList<Integer>(); 
+    		while (rs.next()){
+    			
+    			surNeighbors.add(rs.getInt("neighborID"));
+    		}
+    		rs.close(); 
+    		stat.close();
+    		
+    		List<Integer> invSurNeighbors = new ArrayList<Integer>(); 
+    		query = "SELECT webID FROM SurNeighbors WHERE surID = ?";
+    		stat = null;
+    		rs = null;
+    		node = null;
+    		
+    		stat = db.getConnection().prepareStatement(query);
+    		
+    		stat.setInt(1, webID);
+
+    		rs = stat.executeQuery();
+    	
+    		while (rs.next()){
+    			
+    			invSurNeighbors.add(rs.getInt("webID"));
+    		}
+    		rs.close(); 
+    		stat.close();
+    		
+    		node = new Node(id, height, foldID, surFoldID, invSurFoldID, neighbors, surNeighbors, invSurNeighbors, foldState, insertableState, selfFlat, neighborsFlat, nofneighborsFlat);
+    		
+		
+		
+		}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		// clean up and close
-		rs.close();
-		stat.close();
+
+		
+		
+		//do new query for neighbors
 		
 		return node;
 	}
@@ -64,24 +143,98 @@ public class DatabaseAccessor {
 	 */
 	public boolean addNode(Node node) throws DatabaseException, SQLException  {	
 		// instantiate local variables
-		String query = "INSERT INTO Nodes (webID, height, foldID, surrgFoldID, invSurrgFoldID) VALUES (?,?,?,?,?)";
+		
+		//"insertableState" INTEGER, "selfFlat" INTEGER, "neighborsFlat" INTEGER, "nofneighborsFlat"
+		String query = "INSERT INTO Nodes (webID, height, foldID, surFoldID, invSurFoldID, foldState, insertableState, selfFlat, neighborsFlat, nofneighborsFlat) VALUES (?,?,?,?,?,?,?,?,?,?)";
 		PreparedStatement stat = null;
 		
 		stat =  db.getConnection().prepareStatement(query);
 		// set the variables in the query
         stat.setInt(1, node.getWebID());
         stat.setInt(2, node.getHeight());
-        stat.setInt(3, node.getFold().getWebID());
-        stat.setInt(4, node.getSurrogateFold().getWebID());
-        stat.setInt(5, node.getInvSurrogateFold().getWebID());
-        
+        stat.setInt(3, node.getFoldID());
+        stat.setInt(4, node.getSurrogateFoldID());
+        stat.setInt(5, node.getInvSurrogateFoldID());
+        stat.setInt(6, 1);//foldstate
+        stat.setInt(7, 1);//insertableState
+        stat.setInt(8, 1);//selfFlat
+        stat.setInt(9, 1);//neighborsFlat
+        stat.setInt(10, 1);//nofneighborsFlat
         stat.executeUpdate();
         
         stat.close();  
         
 		return true;
 	}
+	public void clearHyperWeb(){
+		
+		String [] queries = new String[3];
+		queries[0] = "DELETE FROM Nodes";
+		queries[1] = "DELETE FROM Neighbors";
+		queries[2] = "DELETE FROM SurNeighbors";
+		for (int x = 0; x < queries.length; x++) {
+			PreparedStatement stat = null;
+			
+			try {
+				stat = db.getConnection().prepareStatement(queries[x]);
+				stat.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// set the variables in the query
+			
 	
+		
+		}
+	
+		
+	}
+	
+	public HashMap<Integer, Node> loadHyperWeb(){ //return hashmap of integers to nodes
+		//query for all webids in hyperweb
+		//select webID from Nodes;
+		HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+	
+		try {
+			String query = "select webID from Nodes";
+			PreparedStatement stat = null;
+			ResultSet rs = null;
+			
+			stat = db.getConnection().prepareStatement(query);
+			
+			rs = stat.executeQuery();
+			
+				while (rs.next()){
+					nodes.put(rs.getInt("webID"), null);
+				}
+			
+				rs.close();
+				stat.close();
+			
+				//Map<String, Object> map = ...;
+
+				for (Integer i : nodes.keySet()) {
+				    nodes.put(i, getNode(i));
+				}
+			
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DatabaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+		
+
+	
+		
+		return nodes;
+		
+	}
 	/**
 	 * Sends the information of a Node object to the database to update
 	 * 
@@ -91,16 +244,22 @@ public class DatabaseAccessor {
 	 */
 	public boolean updateNode(Node node) throws DatabaseException, SQLException  {
 		// instantiate local variables
-		String query = "UPDATE Nodes SET height = ?, foldID = ?, surFoldID = ?, invSurFoldID = ? WHERE webID = ?";
+		//, "foldState" INTEGER, "insertableState" INTEGER, "selfFlat" INTEGER, "neighborsFlat" INTEGER, "nofneighborsFlat" INTEGER);
+
+		String query = "UPDATE Nodes SET height = ?, foldID = ?, surFoldID = ?, invSurFoldID = ? WHERE webID = ?, foldState = ?, insertableState = ?, selfFlat = ?, neighborsFlat = ?, nofneighborsFlat = ?";
 		PreparedStatement stat = null;
 		
 		stat =  db.getConnection().prepareStatement(query);
 		// set the variables in the query
         stat.setInt(1, node.getHeight());
         stat.setInt(2, node.getFoldID());
-        stat.setInt(3, node.getSurFoldID());
-        stat.setInt(4, node.getInvSurFoldID());
-        
+        stat.setInt(3, node.getSurrogateFoldID());
+        stat.setInt(4, node.getInvSurrogateFoldID());
+        stat.setInt(5, 1);//foldstate
+        stat.setInt(6, 1);//insertableState
+        stat.setInt(7, 1);//selfFlat
+        stat.setInt(8, 1);//neighborsFlat
+        stat.setInt(9, 1);//nofneighborsFlat
         stat.executeUpdate();
         
     	// clean up and close
@@ -144,45 +303,56 @@ public class DatabaseAccessor {
                 
 		return allNodes;
 	}
-
 	
-	public void addNeighbors(int webid, List<int> neighbors){
-		String query = "INSERT INTO Neighbors (webID, height, neighborID) VALUES (?,?)";
+	public boolean addNeighbors(int webid, List<Integer> neighbors) throws DatabaseException, SQLException {
+		String query = "INSERT INTO Neighbors (webID, neighborID) VALUES (?,?)";
 		PreparedStatement stat = null;
 		
-		Iterator<String> iterator = neighbors.iterator();
+		for (int i = 0; i < neighbors.size(); i++) {
+			stat =  db.getConnection().prepareStatement(query);
+			// set the variables in the query
+	        stat.setInt(1, webid);
+	        stat.setInt(2, neighbors.get(i));
+	        
+	        stat.executeUpdate();
+	        stat.close();  
+	        }
+		
+		/* I couldn't get this to work? fix it later if  you want, 
+		 * for loop above does the same - Mychal
+		 */
+		/*
+		Iterator<Integer> iterator = neighbors.iterator();
 		while (iterator.hasNext()) {
 		stat =  db.getConnection().prepareStatement(query);
 		// set the variables in the query
         stat.setInt(1, webid);
         stat.setInt(2, neighbors.next());
-        
-		
-		
 		
         stat.executeUpdate();
         stat.close();  
-        }
+        }*/
+		
 		return true;
 	
 	
 	}
-	public void deleteNode(Node node) {
+	public boolean deleteNode(Node node) throws DatabaseException, SQLException {
 	
 		String [] queries = new String[3];
-		query[0] = "delete from Node where webID = ?";
-		query[1] = "delete from Neighbors where webID = ?";
-		query[2] = "delete from SurNeighbors where webID = ?";
-		for (int x= 0; x<= 2; x++) {
-		PreparedStatement stat = null;
-		ResultSet rs = null;
-		node = null;
-		
-		stat = db.getConnection().prepareStatement(query[x]);
-		// set the variables in the query
-		stat.setInt(1, webID);
-
-		rs = stat.executeUpdate();
+		queries[0] = "DELETE FROM Nodes WHERE webID = ?";
+		queries[1] = "DELETE FROM Neighbors WHERE webID = ?";
+		queries[2] = "DELETE FROM SurNeighbors WHERE webID = ?";
+		for (int x = 0; x < queries.length; x++) {
+			PreparedStatement stat = null;
+			
+			stat = db.getConnection().prepareStatement(queries[x]);
+			// set the variables in the query
+			stat.setInt(1, node.getWebID());
+	
+			stat.executeUpdate();
 		}
-}
+		return true;
+	}
+
 }
