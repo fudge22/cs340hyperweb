b8fddc371e98814184a5e7eb24e17b30456544eb
mykecalderon <mykecalderon@aol.com>
2013-10-09 14:53:47 -0600

    another phase 2 version

---
+++ phase2 again/tests/NodeTests.java
@@ -0,0 +1,46 @@
+package tests;
+import static org.junit.Assert.*;
+
+import model.HyperWeb;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import simulation.Validator;
+
+
+public class NodeTests {
+
+	private static HyperWeb hw;
+	private static Validator v;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		hw = new HyperWeb();	
+		v = new Validator(hw);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void addMoreNodes() {
+		for (int i = 0; i < 100; i++) {
+			hw.addNode();
+			v.validate();
+		}
+	}
+
+}
